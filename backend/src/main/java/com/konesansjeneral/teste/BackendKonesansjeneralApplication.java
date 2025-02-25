cee8d8a1458d42bce97640b425ede3188fb2b22a
Jean Mardochée JOSEPH <josephjeanmardochee@gmail.com>
2025-02-25 01:05:27 +0100

    first commit

---
+++ backend/src/main/java/com/konesansjeneral/teste/BackendKonesansjeneralApplication.java
@@ -0,0 +1,13 @@
+package com.konesansjeneral.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKonesansjeneralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendKonesansjeneralApplication.class, args);
+	}
+
+}
